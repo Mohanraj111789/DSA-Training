a91c9babc1d18789926d5cbccb7e5cebafbbe7fc
Mohanraj111789 <kit27.am30@gmail.com>
2025-12-15 12:14:28 +0530

    top view and bottom view of BT dfs and bfs

--- Binary_Tree/topviewofBT.java
+++ Binary_Tree/topviewofBT.java
@@ -11,32 +11,83 @@ class Node {
 }
 */
 class Solution {
-    int min_col = 0;
-    int max_col = 0;
-    public void preOrder(Node root,int row,int col,HashMap<Integer,int[]>mp)
-    {
-        if(root == null)
-            return;
-        if(!mp.containsKey(col) || mp.get(col)[1] > row)
-            mp.put(col,new int[]{root.data,row});
- 
-        if(max_col < col)
-            max_col = col;
-        if(min_col > col)
-            min_col = col;
-        preOrder(root.left,row+1,col-1,mp);
-        preOrder(root.right,row+1,col+1,mp);
+    int minc=0;
+    int maxc=0;
+   void preorder(Node root,int row,int col,HashMap<Integer,int[]> mp){
+       if(root!=null){
+           if(!mp.containsKey(col) || row<mp.get(col)[1]){
+               mp.put(col,new int[]{root.data,row});
+           }
+           minc=Math.min(minc,col);
+           maxc=Math.max(maxc,col);
+           preorder(root.left,row+1,col-1,mp);
+           preorder(root.right,row+1,col+1,mp);
+       }
+   }
+    public ArrayList<Integer> topView(Node root) {
+        // code here
+        HashMap<Integer,int[]> mp= new HashMap<>();
+        preorder(root,0,0,mp);
+        ArrayList<Integer> left = new ArrayList<>();
+        for(int i=minc;i<=maxc;i++){
+            left.add(mp.get(i)[0]);
+        }
+        return left;
+    }
+    
+}
+
+//bfs approach;
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        this.data = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+class Solution {
+    public class Pair{
+        Node node;
+        int left;
+        int right;
+        Pair(Node node,int left,int right){
+            this.node=node;
+            this.left=left;
+            this.right=right;
+        }
     }
     public ArrayList<Integer> topView(Node root) {
         // code here
-        HashMap<Integer,int[]>mp = new HashMap<>();
-        preOrder(root,0,0,mp);
-        ArrayList<Integer>ans = new ArrayList<>();
-        for(int i=min_col;i<=max_col;i++)
-        {
-            ans.add(mp.get(i)[0]);
+        ArrayList<Integer> left = new ArrayList<>();
+        ArrayList<Integer> right = new ArrayList<>();
+        Deque<Pair> que = new ArrayDeque<>();
+        int leftmost=0;
+        int rightmost=0;
+        que.addLast(new Pair(root,1,0));
+        while(!que.isEmpty()){
+            Pair cur = que.removeFirst();
+            Node node = cur.node;
+            int cleft = cur.left;
+            int cright = cur.right;
+            if(cleft>leftmost){
+                left.add(node.data);
+                leftmost = cleft;
+            }
+            if(cright>rightmost){
+                right.add(node.data);
+                rightmost = cright;
+            }
+            if(node.left!=null) que.addLast(new Pair(node.left,cleft+1,cright-1));
+            if(node.right!=null) que.addLast(new Pair(node.right,cleft-1,cright+1));
         }
-        return ans;
-        
+        Collections.reverse(left);
+        left.addAll(right);
+        return left;
     }
+    
 }
