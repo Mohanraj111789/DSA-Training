099622a9a17e917014f2a4e1c58046db63c5c231
Mohanraj111789 <kit27.am30@gmail.com>
2025-12-15 14:35:22 +0530

    path to given Node

---
+++ Binary_Tree/Solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    ArrayList<Integer>result = new ArrayList<>();
+    public boolean preorder(TreeNode root,int data)
+    {
+        if(root == null)
+            return  false;
+        result.add(root.val);
+        if(root.val == data)
+            return true;
+        if(preorder(root.left,data) || preorder(root.right,data))
+            return true;
+        result.remove(result.size()-1);
+        return false;
+        
+    }
+    public ArrayList<Integer>solve(TreeNode A, int B) {
+        preorder(A,B);
+        return result;
+        
+    }
+}
