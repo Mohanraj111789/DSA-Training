882045b4da01d16b9dd1bf51c6fffdfd29f2252c
Mohanraj111789 <kit27.am30@gmail.com>
2025-12-23 11:27:09 +0530

    Graph Cycle detection khan's Algorithm

--- Graphs/Topologicalsort/cycleDetectionBFS.java
+++ Graphs/Topologicalsort/cycleDetectionBFS.java
@@ -1,43 +1,40 @@
 class Solution {
     public boolean isCyclic(int V, int[][] edges) {
         // code here
+        int [] indegree = new int[V];
         ArrayList<ArrayList<Integer>>adj = new ArrayList<>();
         for(int i=0;i<V;i++)
-        {
             adj.add(new ArrayList<>());
-        }
-        for(int[] edge:edges)
+        
+        for(int[]edge:edges)
         {
             adj.get(edge[0]).add(edge[1]);
+            indegree[edge[1]]++;
         }
-        int indegree[] = new int[V];
-        int vis = 0;
-        for(int i=0;i<V;i++)
-        {
-            for(int ne:adj.get(i))
-            {
-                indegree[ne]++;
-            }
-        }
-
-        Queue<Integer> q = new LinkedList<>();
+        
+        int count =0;
+        Queue<Integer>q = new LinkedList<>();
         for(int i=0;i<V;i++)
         {
             if(indegree[i] == 0)
                 q.add(i);
         }
+        
         while(!q.isEmpty())
         {
-            int x = q.poll();
-            vis++;
-            for(int ne:adj.get(x))
+            count++;
+            int cur = q.poll();
+            for(int ne:adj.get(cur))
             {
                 indegree[ne]--;
                 if(indegree[ne] == 0)
                     q.add(ne);
             }
         }
-        return vis != V;
+        return count != V;
+        
+        
+        
     }
 }
 //problem link: https://practice.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1  access ctrl+click
